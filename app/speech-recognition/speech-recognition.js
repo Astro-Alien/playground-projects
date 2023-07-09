a80ace062c42d5c0f0e45cc407203e2748837095
CharlesLemmert <charles.lemmert12345@gmail.com>
2023-07-09 19:05:08 +0200

    committing test

--- app/speech-recognition/speech-recognition.js
+++ app/speech-recognition/speech-recognition.js
@@ -1,4 +1,5 @@
 export default class SpeechRecognition extends crs.classes.BindableElement {
+      #speechHandler;
 
       get html() {
         return import.meta.url.replace(".js", ".html");
@@ -6,11 +7,49 @@ export default class SpeechRecognition extends crs.classes.BindableElement {
 
       async connectedCallback() {
         await super.connectedCallback();
-        console.log("hello world");
+        await this.load();
       }
 
       async disconnectedCallback() {
         await super.disconnectedCallback();
+      }
+
+      load() {
+        return new Promise((resolve) =>{
+            requestAnimationFrame(async ()=> {
+                await this.#startingSpeechListener();
+                resolve();
+            });
+        });
+
+      }
+
+
+      async  #startingSpeechListener() {
+         const speech = true;
+         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+         const recognition = new SpeechRecognition();
+
+         SpeechRecognition.interimResults = true;
+
+         recognition.addEventListener("result", (event) => {
+                const transcript = Array.from(event.results).map(result => result[0])
+                .map(result => result.transcript);
+
+                const fn = transcript[0];
+
+                if (this[fn] != null) {
+                    this[fn]();
+                }
+         });
+
+         if (speech === true) {
+            recognition.start();
+         }
+      }
+
+      async print() {
+          console.log("hello world");
       }
 
 }
